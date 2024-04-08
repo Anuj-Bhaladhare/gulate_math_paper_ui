9ae1da9f49196149a938522499e37319066b89f4
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2024-04-08 16:44:11 +0530

    udate

---
+++ src/pages/singup/hook.js
@@ -0,0 +1,37 @@
+import relayService from "../../axios/hook";
+import { useNavigate } from 'react-router-dom';
+
+const useSingUp = () => {
+
+    const navigate = useNavigate();
+    
+    const userSingUp = async (initialState) => {
+        try{
+            const { firstName, lastName, email, password, role } = initialState;
+
+            const response = await relayService({
+                url: '/signup',
+                method: 'POST',
+                data: {
+                    firstName,
+                    lastName,
+                    email,
+                    password,
+                    role
+                }
+            });
+
+            if(response?.status === 200) {
+                console.log("user created success", response);
+                navigate('/login');
+                return response.data;
+            }
+        } catch (err) {
+
+        }
+    }
+      
+    return [{ userSingUp }];
+}
+
+export default useSingUp;
